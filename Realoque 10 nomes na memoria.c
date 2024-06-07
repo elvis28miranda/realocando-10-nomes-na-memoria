1caddefab6d93ecbd6834ed79d19108e61056b0f
Elvis Miranda <123380523+elvis28miranda@users.noreply.github.com>
2024-06-07 17:12:30 -0300

    Add files via upload

---
+++ Realoque 10 nomes na memoria.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdio.h>
+
+typedef struct no
+{
+    char nome [10];
+}no;
+
+struct no *A;
+int i;
+
+int main()
+{
+
+for(i=0;i<10;i++){
+
+    printf("\n Informe o nome: ");
+    scanf("%s", A -> nome);
+
+
+  /*  A= (nome) malloc (sizeof (nome));*/
+}
+
+return 0;
+}
+
+
